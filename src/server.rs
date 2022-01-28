0d252987298284cf22312d16753c010c94d11725
max <36980911+pr2502@users.noreply.github.com>
2022-01-29 00:51:34 +0100

    attempt to solve initial handshake messages
    - there is still more synchronization required, new client receives the
      response and notification before they even send a request
    - disconnecting client breaks the server
    - rust-anayzer throws an error when a second client connects (about
      unhandled "initialized" notification?), this might be also caused
      by the lack of synchronization

--- src/server.rs
+++ src/server.rs
@@ -13,20 +13,20 @@ use ra_multiplex::proto;
 use serde_json::{Map, Number, Value};
 use server::lsp;
 use server::message::Message;
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::net::Ipv4Addr;
 use std::path::{Path, PathBuf};
 use std::process::Stdio;
 use std::str::{self, FromStr};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::collections::hash_map::Entry;
 use tokio::io::{
     AsyncBufRead, AsyncBufReadExt, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader,
 };
 use tokio::net::{TcpListener, TcpStream};
 use tokio::process::{Child, Command};
-use tokio::sync::RwLock;
-use tokio::sync::{mpsc, Mutex};
+use tokio::sync::{mpsc, watch, Mutex, RwLock};
 use tokio::task;
 
 mod server {
@@ -34,9 +34,70 @@ mod server {
     pub mod message;
 }
 
+struct InitializeCache {
+    request_sent: AtomicBool,
+    response_send: watch::Sender<Option<Message>>,
+    response_recv: watch::Receiver<Option<Message>>,
+    notif_send: watch::Sender<Option<Message>>,
+    notif_recv: watch::Receiver<Option<Message>>,
+}
+
+impl Default for InitializeCache {
+    fn default() -> Self {
+        let (response_send, response_recv) = watch::channel(None);
+        let (notif_send, notif_recv) = watch::channel(None);
+        InitializeCache {
+            request_sent: AtomicBool::new(false),
+            response_send,
+            response_recv,
+            notif_send,
+            notif_recv,
+        }
+    }
+}
+
+impl InitializeCache {
+    async fn wait_for_response(&self) -> Message {
+        let mut receiver = self.response_recv.clone();
+        loop {
+            if let Some(notif) = receiver.borrow().as_ref() {
+                return notif.clone();
+            }
+            receiver.changed().await.unwrap();
+        }
+    }
+
+    fn send_response(&self, message: Message) {
+        self.response_send.send(Some(message)).unwrap();
+    }
+
+    async fn wait_for_notif(&self) -> Message {
+        let mut receiver = self.notif_recv.clone();
+        loop {
+            if let Some(notif) = receiver.borrow().as_ref() {
+                return notif.clone();
+            }
+            receiver.changed().await.unwrap();
+        }
+    }
+
+    fn send_notif(&self, message: Message) {
+        self.notif_send.send(Some(message)).unwrap();
+    }
+
+    /// returns true if this is the first thread attempting to send a request
+    fn attempt_send_request(&self) -> bool {
+        let already_sent = self.request_sent.swap(true, Ordering::SeqCst);
+        !already_sent
+    }
+}
+
+type MessageReaders = RwLock<HashMap<u16, mpsc::Sender<Message>>>;
+
 struct RaInstance {
-    message_readers: Arc<RwLock<HashMap<u16, mpsc::Sender<Message>>>>,
+    message_readers: Arc<MessageReaders>,
     message_writer: mpsc::Sender<Message>,
+    init_cache: Arc<InitializeCache>,
     child: Child,
 }
 
@@ -74,13 +135,16 @@ impl RaInstance {
             log::info!("[{path}] read_server_errors closed");
         });
 
+        let init_cache = Arc::new(InitializeCache::default());
+        let message_readers = Arc::new(MessageReaders::default());
+
         // read messages from server stdout and distribute them to client channels
         let path = project_root.display().to_string();
         let stdout = BufReader::new(child.stdout.take().unwrap());
-        let message_readers = Arc::new(RwLock::new(HashMap::new()));
         let message_readers2 = Arc::clone(&message_readers);
+        let init_cache2 = Arc::clone(&init_cache);
         task::spawn(async move {
-            if let Err(err) = read_server_socket(stdout, message_readers2).await {
+            if let Err(err) = read_server_socket(stdout, message_readers2, init_cache2).await {
                 log::error!("[{path}] read_server_socket {err:?}");
             }
             log::info!("[{path}] read_server_socket closed");
@@ -101,12 +165,17 @@ impl RaInstance {
             child,
             message_readers,
             message_writer,
+            init_cache,
         })
     }
 }
 
 /// finds or spawns a rust-analyzer instance and connects the client
-async fn process_client(socket: TcpStream, port: u16, registry: Arc<Mutex<HashMap<PathBuf, RaInstance>>>) -> Result<()> {
+async fn process_client(
+    socket: TcpStream,
+    port: u16,
+    registry: Arc<Mutex<HashMap<PathBuf, RaInstance>>>,
+) -> Result<()> {
     log::info!("accepted {port}");
 
     let (socket_read, socket_write) = socket.into_split();
@@ -121,23 +190,26 @@ async fn process_client(socket: TcpStream, port: u16, registry: Arc<Mutex<HashMa
 
     let proto_init: proto::Init = serde_json::from_slice(&buffer).context("invalid proto init")?;
     ensure!(proto_init.check_version(), "invalid protocol version");
-    ensure!(proto_init.args.is_empty(), "unexpected args passed to client");
+    ensure!(
+        proto_init.args.is_empty(),
+        "unexpected args passed to client"
+    );
 
     let project_root = find_project_root(&proto_init.cwd)
         .with_context(|| format!("couldn't find project root for {}", &proto_init.cwd))?;
 
-    // TODO for now we're spawning a new instance unconditionally
     let mut registry = registry.lock().await;
     let instance = match registry.entry(project_root.clone()) {
         Entry::Occupied(e) => e.into_mut(),
-        Entry::Vacant(e) => e.insert(
-        RaInstance::spawn(&project_root).context("spawn ra instance")?
-        ),
+        Entry::Vacant(e) => {
+            e.insert(RaInstance::spawn(&project_root).context("spawn ra instance")?)
+        }
     };
 
     let tx = instance.message_writer.clone();
+    let init_cache = Arc::clone(&instance.init_cache);
     task::spawn(async move {
-        if let Err(err) = read_client_socket(socket_read, tx, port).await {
+        if let Err(err) = read_client_socket(socket_read, tx, port, init_cache).await {
             log::error!("read_client_socket {err:?}");
         }
         log::info!("read_client_socket closed");
@@ -145,7 +217,7 @@ async fn process_client(socket: TcpStream, port: u16, registry: Arc<Mutex<HashMa
 
     let (tx, rx) = mpsc::channel(64);
     let mut message_readers = instance.message_readers.write().await;
-    message_readers.insert(port, tx);
+    message_readers.insert(port, tx.clone());
     drop(message_readers);
     task::spawn(async move {
         if let Err(err) = write_socket(rx, socket_write).await {
@@ -153,6 +225,17 @@ async fn process_client(socket: TcpStream, port: u16, registry: Arc<Mutex<HashMa
         }
         log::info!("write_socket(client) closed");
     });
+
+    let init_cache = Arc::clone(&instance.init_cache);
+    drop(registry); // unlock the registry before the next await point
+
+    let message = init_cache.wait_for_response().await;
+    log::info!("send response to InitializeRequest for {port}");
+    tx.send(message).await.unwrap();
+
+    let message = init_cache.wait_for_notif().await;
+    log::info!("send InitializedNotification for {port}");
+    tx.send(message).await.unwrap();
     Ok(())
 }
 
@@ -162,6 +245,7 @@ async fn read_client_socket<R>(
     mut reader: R,
     sender: mpsc::Sender<Message>,
     port: u16,
+    init_cache: Arc<InitializeCache>,
 ) -> Result<()>
 where
     R: AsyncBufRead + Unpin,
@@ -182,6 +266,32 @@ where
 
         let mut json: Map<String, Value> =
             serde_json::from_slice(&buffer).context("invalid body")?;
+
+        if let Some(Value::String(method)) = json.get("method") {
+            if method == "initialize" {
+                log::info!("recv InitializeRequest from {port}");
+                // this is an initialize request, it's special because it cannot
+                if init_cache.attempt_send_request() {
+                    // it haven't been sent yet, we can send it.
+
+                    // instead of taggin the original id we replace it with a custom id that only
+                    // the `initialize` uses
+                    json.insert("id".to_owned(), Value::String("initialize_request".to_owned()));
+                    buffer.clear();
+                    serde_json::to_writer(&mut buffer, &json).context("serialize body")?;
+
+                    sender
+                        .send(Message::new(&buffer))
+                        .await
+                        .context("forward client request")?;
+                }
+
+                // we skip further processing regardless of whether we forwarded the initialize
+                // request or not
+                continue;
+            }
+        }
+
         if let Some(id) = json.get("id") {
             log::debug!("recv request port={port}, message_id={id:?}");
 
@@ -232,7 +342,8 @@ fn parse_tagged_id(tagged: &str) -> Result<(u16, Value)> {
 
 async fn read_server_socket<R>(
     mut reader: R,
-    senders: Arc<RwLock<HashMap<u16, mpsc::Sender<Message>>>>,
+    senders: Arc<MessageReaders>,
+    init_cache: Arc<InitializeCache>,
 ) -> Result<()>
 where
     R: AsyncBufRead + Unpin,
@@ -255,9 +366,31 @@ where
 
         let mut json: Map<String, Value> =
             serde_json::from_slice(&buffer).context("invalid body")?;
+
+        if let Some(Value::String(method)) = json.get("method") {
+            if method == "initialized" {
+                // initialized notification needs to be cached so it can be sent to every new
+                // client
+                log::debug!("recv InitializedNotification");
+                init_cache.send_notif(Message::new(&buffer));
+                continue;
+            }
+        }
+
         if let Some(id) = json.get("id") {
             // we tagged the request id so we expect to only receive tagged responses
             let tagged_id = match id {
+                Value::String(string) if string == "initialize_request" => {
+                    // this is a response to the InitializeRequest, we need to process it
+                    // separately
+                    log::debug!("recv InitializeRequest response");
+                    // NOTE we just guess the first request had originally the id Number(0)
+                    json.insert("id".to_owned(), Value::Number(0.into()));
+                    buffer.clear();
+                    serde_json::to_writer(&mut buffer, &json).context("serialize body")?;
+                    init_cache.send_response(Message::new(&buffer));
+                    continue;
+                }
                 Value::String(string) => string,
                 _ => {
                     log::warn!("unexpected response message id type {id:?}");
