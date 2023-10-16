72b8327a50af08ca811745b44b7fc34ab6e8bc19
max <36980911+pr2502@users.noreply.github.com>
2023-10-17 00:26:56 +0200

    use tokio::io::copy_bidirectional in client

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,52 @@
-use anyhow::{Context, Result};
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use anyhow::{Context as _, Result};
+use pin_project_lite::pin_project;
 use ra_multiplex::config::Config;
 use ra_multiplex::proto;
-use tokio::io::AsyncWriteExt;
+use tokio::io::{self, AsyncRead, AsyncWrite, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio::{io, task};
+
+pin_project! {
+    struct Stdio {
+        #[pin]
+        stdin: io::Stdin,
+        #[pin]
+        stdout: io::Stdout,
+    }
+}
+
+fn stdio() -> Stdio {
+    Stdio {
+        stdin: io::stdin(),
+        stdout: io::stdout(),
+    }
+}
+
+impl AsyncRead for Stdio {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context,
+        buf: &mut io::ReadBuf,
+    ) -> Poll<io::Result<()>> {
+        self.project().stdin.poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for Stdio {
+    fn poll_write(self: Pin<&mut Self>, cx: &mut Context, buf: &[u8]) -> Poll<io::Result<usize>> {
+        self.project().stdout.poll_write(cx, buf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
+        self.project().stdout.poll_flush(cx)
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
+        self.project().stdout.poll_shutdown(cx)
+    }
+}
 
 pub async fn main(server_path: String, server_args: Vec<String>) -> Result<()> {
     let config = Config::load_or_default().await;
@@ -12,31 +55,18 @@ pub async fn main(server_path: String, server_args: Vec<String>) -> Result<()> {
     let mut proto_init = serde_json::to_vec(&proto_init).context("sending proto init")?;
     proto_init.push(b'\0');
 
-    let stream = TcpStream::connect(config.connect)
+    let mut stream = TcpStream::connect(config.connect)
         .await
         .context("connect")?;
-    let (mut read_stream, mut write_stream) = stream.into_split();
 
-    write_stream
+    stream
         .write_all(&proto_init)
         .await
         .context("sending proto init")?;
     drop(proto_init);
 
-    let t1 = task::spawn(async move {
-        io::copy(&mut read_stream, &mut io::stdout())
-            .await
-            .context("io error")
-    });
-    let t2 = task::spawn(async move {
-        io::copy(&mut io::stdin(), &mut write_stream)
-            .await
-            .context("io error")
-    });
-    tokio::select! {
-        res = t1 => res,
-        res = t2 => res,
-    }
-    .context("join")??;
+    io::copy_bidirectional(&mut stream, &mut stdio())
+        .await
+        .context("io error")?;
     Ok(())
 }
