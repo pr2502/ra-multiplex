5c647e04d58c1999495a85aa58ceae09d5f1da21
max <36980911+pr2502@users.noreply.github.com>
2023-10-20 22:34:17 +0200

    check protocol version and enable more commands in the future

--- examples/neovim/init.lua
+++ examples/neovim/init.lua
@@ -2,8 +2,9 @@ require'lspconfig'.rust_analyzer.setup {
   cmd = vim.lsp.rpc.connect("127.0.0.1", 27631),
   init_options = {
     lspMux = {
-      server = "rust-analyzer",
       version = "1",
+      method = "connect",
+      server = "rust-analyzer",
     },
   },
   settings = {
