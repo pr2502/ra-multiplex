e7cd6096c1e1ace6b651a324d0e7e5af9962dc6f
max <36980911+pr2502@users.noreply.github.com>
2024-05-16 21:11:28 +0200

    update tokio and replace custom implementation of tokio::io::join

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,11 +1,8 @@
 use std::collections::BTreeMap;
 use std::env;
-use std::pin::Pin;
-use std::task::{Context, Poll};
 
 use anyhow::{bail, Context as _, Result};
-use pin_project_lite::pin_project;
-use tokio::io::{self, AsyncRead, AsyncWrite, BufStream};
+use tokio::io::{self, BufStream};
 use tokio::net::TcpStream;
 
 use crate::config::Config;
@@ -13,46 +10,6 @@ use crate::lsp::ext::{LspMuxOptions, Request};
 use crate::lsp::jsonrpc::Message;
 use crate::lsp::transport::{LspReader, LspWriter};
 use crate::lsp::{InitializationOptions, InitializeParams};
-
-pin_project! {
-    struct Stdio {
-        #[pin]
-        stdin: io::Stdin,
-        #[pin]
-        stdout: io::Stdout,
-    }
-}
-
-fn stdio() -> Stdio {
-    Stdio {
-        stdin: io::stdin(),
-        stdout: io::stdout(),
-    }
-}
-
-impl AsyncRead for Stdio {
-    fn poll_read(
-        self: Pin<&mut Self>,
-        cx: &mut Context,
-        buf: &mut io::ReadBuf,
-    ) -> Poll<io::Result<()>> {
-        self.project().stdin.poll_read(cx, buf)
-    }
-}
-
-impl AsyncWrite for Stdio {
-    fn poll_write(self: Pin<&mut Self>, cx: &mut Context, buf: &[u8]) -> Poll<io::Result<usize>> {
-        self.project().stdout.poll_write(cx, buf)
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
-        self.project().stdout.poll_flush(cx)
-    }
-
-    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
-        self.project().stdout.poll_shutdown(cx)
-    }
-}
 
 pub async fn run(config: &Config, server: String, args: Vec<String>) -> Result<()> {
     let cwd = env::current_dir()
@@ -69,7 +26,7 @@ pub async fn run(config: &Config, server: String, args: Vec<String>) -> Result<(
     let mut stream = TcpStream::connect(config.connect)
         .await
         .context("connect")?;
-    let mut stdio = BufStream::new(stdio());
+    let mut stdio = BufStream::new(io::join(io::stdin(), io::stdout()));
 
     // Wait for the client to send `initialize` request.
     let mut reader = LspReader::new(&mut stdio, "client");
