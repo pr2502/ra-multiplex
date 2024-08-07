cd9128982c2ba87d0052ccd49a1254e30980681d
yuanyuyuan <az6980522@gmail.com>
2024-08-07 12:46:31 +0800

    fix: `init_options` is no longer supported in the rust-analyzer lsp config
    As lspconfig states in the doc,
    https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#rust_analyzer
    we need to pass the additional lspMux in the settings instead. This PR
    tries to fix the example config for neovim LSP.

--- examples/neovim/init.lua
+++ examples/neovim/init.lua
@@ -2,11 +2,13 @@ require'lspconfig'.rust_analyzer.setup {
   cmd = vim.lsp.rpc.connect("127.0.0.1", 27631),
   -- When using unix domain sockets, use something like:
   --cmd = vim.lsp.rpc.connect("/path/to/ra-multiplex.sock"),
-  init_options = {
-    lspMux = {
-      version = "1",
-      method = "connect",
-      server = "rust-analyzer",
+  settings = {
+    ["rust-analyzer"] = {
+      lspMux = {
+        version = "1",
+        method = "connect",
+        server = "rust-analyzer",
+      },
     },
   },
 }
